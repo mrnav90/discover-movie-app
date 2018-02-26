9c8854fb28a29fd15337f8cebaaffb599f136d88
VietNa <mr.nav90@gmail.com>
2018-02-27 01:58:06 +0700

    update scroll view

--- app/screens/Discover/DiscoverScreen.js
+++ app/screens/Discover/DiscoverScreen.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import ScrollableTabView from 'react-native-scrollable-tab-view';
-import ScrollableTabBar from '../../components/ScrollableTabBar';
 import DiscoverListView from '../../components/DiscoverListView';
+import { TabViewAnimated, TabBar } from 'react-native-tab-view';
+import {
+  Dimensions
+} from 'react-native';
 
 export class DiscoverScreen extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      index: 0,
+      routes: [
+        { key: 'movie', title: 'Movies' },
+        { key: 'tvshow', title: 'TV Shows' }
+      ]
+    };
+  }
+
+  onIndexChange = (index) => this.setState({ index });
+
+  renderHeader = (props) => {
+    return <TabBar
+      style={{backgroundColor: 'white'}}
+      indicatorStyle={{backgroundColor: '#F08576', height: 1}}
+      tabStyle={{opacity: 1}}
+      labelStyle={{color: '#F08576'}}
+      {...props}
+    />;
+  }
+
+  renderScene = ({ route }) => {
+    switch (route.key) {
+      case 'movie':
+        return <DiscoverListView type="movie"/>;
+      case 'tvshow':
+        return <DiscoverListView type="tvshow"/>;
+      default:
+        return null;
+    }
   }
 
   render() {
-    return <ScrollableTabView
-      initialPage={0}
-      style={{backgroundColor: 'white'}}
-      locked={true}
-      tabBarBackgroundColor="white"
-      tabBarActiveTextColor="#F08576"
-      tabBarInactiveTextColor="#A2A2A2"
-      renderTabBar={() => <ScrollableTabBar underlineStyle={{height: 1, backgroundColor: '#F08576'}} />}
-    >
-      <DiscoverListView type="movie" tabLabel="Movies"/>
-      <DiscoverListView type="tvshow" tabLabel="TV Shows"/>
-    </ScrollableTabView>;
+    return <TabViewAnimated
+        style={{flex: 1}}
+        navigationState={this.state}
+        renderScene={this.renderScene}
+        renderHeader={this.renderHeader}
+        onIndexChange={this.onIndexChange}
+        initialLayout={{width: Dimensions.get('window').width, height: 0}}
+      />;
   }
 }
