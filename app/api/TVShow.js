d5025b1d4586b1e1cf8b3fc994476e93b0b2273e
Na Ngo Po <na.np@workhouse.me>
2018-02-27 22:28:44 +0700

    Added TV Tab

---
+++ app/api/TVShow.js
@@ -0,0 +1,48 @@
+import reduxAPI from 'redux-api';
+import adapterFetch from 'redux-api/lib/adapters/fetch';
+import { API_URL, API_KEY } from '../constants';
+
+export default reduxAPI({
+  airing_today: {
+    url: `tv/airing_today?api_key=${API_KEY}`,
+    options: {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  },
+  on_the_air: {
+    url: `tv/on_the_air?api_key=${API_KEY}`,
+    options: {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  },
+  popular: {
+    url: `tv/popular?api_key=${API_KEY}`,
+    options: {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  },
+  top_rated: {
+    url: `tv/top_rated?api_key=${API_KEY}`,
+    options: {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+})
+.use('fetch', adapterFetch(fetch))
+.use('rootUrl', API_URL);
