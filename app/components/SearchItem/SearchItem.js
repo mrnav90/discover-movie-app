3ca27a83248416904171221e451e3dcb99fe72c2
MrNav <viet.na@neo-lab.vn>
2018-03-12 17:24:59 +0700

    show list search results movie and tv show

--- app/components/SearchItem/SearchItem.js
+++ app/components/SearchItem/SearchItem.js
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Dimensions,
   findNodeHandle,
   StyleSheet
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { HOST_IMAGE } from '../../constants';
 import FastImage from 'react-native-fast-image';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import * as Progress from 'react-native-progress';
 import { createImageProgress } from 'react-native-image-progress';
-import { BlurView } from 'react-native-blur';
 import DateTime from '../DateTime';
+import { ShowIf } from '../../utils';
 
 export default class SearchItem extends Component {
 
@@ -44,10 +42,42 @@ export default class SearchItem extends Component {
   }
 
   render() {
+    const ImageCache = createImageProgress(FastImage);
+    const imagePath = this.props.poster_path ? this.props.poster_path : this.props.backdrop_path;
+    let postTitle = null;
+    let postDate = null;
+    switch (this.props.media_type) {
+      case 'movie':
+        postTitle = this.props.title;
+        postDate = this.props.release_date;
+        break;
+      case 'tv':
+        postTitle = this.props.name;
+        postDate = this.props.first_air_date;
+        break;
+      default:
+        postTitle = this.props.name;
+        postDate = this.props.first_air_date;
+        break;
+    }
     return (
-      <View style={styles.container}>
-        <Text>{this.props.name}</Text>
-      </View>
+      <ShowIf condition={imagePath !== null}>
+        <View style={styles.container}>
+          <ImageCache
+            ref={(img) => { this.backgroundImage = img; }}
+            onLoadEnd={this.imageLoaded}
+            indicator={Progress.Pie}
+            resizeMode="contain"
+            style={{width: 135}}
+            source={{uri: HOST_IMAGE + 'w185' + imagePath}}
+          />
+          <View style={styles.itemInfo}>
+            <Text style={styles.title}>{postTitle}</Text>
+            <DateTime style={styles.date} date={postDate} format="MMMM D, YYYY"/>
+            <Text numberOfLines={7} ellipsizeMode="tail" style={styles.description}>{this.props.overview}</Text>
+          </View>
+        </View>
+      </ShowIf>
     );
   }
 }
@@ -57,6 +87,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     height: 200,
-    marginTop: 10
+    marginTop: 10,
+    flexDirection: 'row'
+  },
+  itemInfo: {
+    padding: 10
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    width: 200
+  },
+  date: {
+    color: '#B5B5B5',
+    marginTop: 5
+  },
+  description: {
+    marginTop: 5,
+    width: 200
   }
 });
