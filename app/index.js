c4377a4d6bc545615fe0b468d31a63fe56087650
Mr. Nav <mr.nav90@gmail.com>
2018-03-24 19:56:49 +0700

    fix bug crash app: Release mode - uglify-es

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { ApplicationNavigator } from './navigators';
+import asyncToGenerator from 'async-to-generator';
+
+babelHelpers.asyncToGenerator = asyncToGenerator;
 
 export default class Application extends Component {
   render() {
